d8783b4fae673142285fdadfe418086520b06d8d
jon <jonathan.lazarini@wipro.com>
2017-02-08 18:32:36 +0000

    fix linting errors

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 /**
  * Created by jonlazarini on 08/02/17.
  */
-var express = require('express');
-var app = express();
+const express = require('express');
+
+const app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist`));
 
-app.set('views', __dirname + '/src/views');
-//TODO Replace with other templating engine... ejs is ugly T_T
+app.set('views', `${__dirname}/src/views`);
+// TODO Replace with other templating engine... ejs is ugly T_T
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-    response.send('HELLO WORLD!');
+app.get('/', (request, response) => {
+  response.send('HELLO WORLD!');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
 });
