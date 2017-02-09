bfaa5eead41c0e365b0bab9082c496953f6be858
jon <jonathan.lazarini@wipro.com>
2017-02-09 19:43:40 +0000

    remove dotenv in app.js for travis build

--- app.js
+++ app.js
@@ -4,14 +4,12 @@
 const express = require('express');
 const path = require('path');
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 
 // Grab npm_package_version env var from npm when running server script
 const PKG_VERSION = process.env.PKG_VERSION;
-
-console.log(process.env);
 
 app.set('port', (process.env.PORT || 5000));
 
