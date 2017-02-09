a88438ae7dc334b3b86512c2f9763d5041f31e95
jon <jonathan.lazarini@wipro.com>
2017-02-09 16:32:15 +0000

    refactor js build script with babelify, browserify, remove static dist build for pug, change Procfile for Heroku

--- app.js
+++ app.js
@@ -2,20 +2,33 @@
  * Created by jonlazarini on 08/02/17.
  */
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const fs = require('fs');
 
 const app = express();
 
 
+// Grab npm_package_version env var from npm when running server script
+const PKG_VERSION = process.env.PKG_VERSION;
+
+dotenv.load();
+// Writes .env file from .sample-env
+fs.createReadStream('.sample-env')
+    .pipe(fs.createWriteStream('.env'));
+
+console.log(path.join(__dirname, `/dist/${PKG_VERSION}`));
+
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(`${__dirname}/dist`));
-app.set('views', `${__dirname}/src/views`);
-
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/src/views'));
+
+app.use(express.static(path.join(__dirname, `/dist/${PKG_VERSION}`)));
 
 app.get('/', (req, res) => {
   const data = { foo: 'bar' };
-  res.render('index', { data: data.foo });
+  res.render('index', { data });
 });
 
 app.listen(app.get('port'), () => {
